1cc47ab9fe271a5a5b887cadfa2d56e91e6144bf
Timur <Stafr95@mail.ru>
2023-09-17 20:27:43 +0500

    npm jquery installation

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+console.log(1);
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
