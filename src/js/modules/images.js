573aedac0d4b7b99036218d42673d8ba7497b4c6
Timur <Stafr95@mail.ru>
2023-09-20 20:02:49 +0500

    completion of the implementation of the module with displaying pictures

---
+++ src/js/modules/images.js
@@ -0,0 +1,50 @@
+const images = () => {
+	const imgPopup = document.createElement('div'),
+		  workSection = document.querySelector('.works'),
+		  bigImage = document.createElement('img');
+
+	imgPopup.classList.add('popup');
+	workSection.appendChild(imgPopup);
+
+	imgPopup.style.justifyContent = 'center';
+	imgPopup.style.alignItems = 'center';
+	imgPopup.style.display = 'none';
+
+
+	bigImage.style.cssText = `
+		width: 550px;
+		height: 550px;
+	`;
+
+	workSection.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		let target = e.target;
+
+		if (target && target.classList.contains('preview')) {
+			imgPopup.style.display = 'flex';
+
+			document.body.style.overflow = 'hidden';
+
+			const path = target.parentNode.getAttribute('href');
+			bigImage.setAttribute('src', path);
+		}
+
+		if (target && target.matches('div.popup')) {
+			imgPopup.style.display = 'none';
+
+			document.body.style.overflow = '';
+		}
+
+		document.addEventListener('keydown', (e) => {
+			if (e.code === 'Escape' || target && target.matches('div.popup')) {
+				document.body.style.overflow = '';
+				imgPopup.style.display = 'none';
+			}
+		})
+	})
+
+	imgPopup.appendChild(bigImage);
+};
+
+export default images;
